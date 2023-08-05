2f6ff421cf26ec8f0b8b0083efa247ed981bff71
Roman Saraev <saraevrom@gmail.com>
2023-08-05 19:48:23 +0300

    Added line style validation for tweakable lines

--- localized_GUI/signal_plotter/style_dialog.py
+++ localized_GUI/signal_plotter/style_dialog.py
@@ -1,7 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.simpledialog import Dialog
 from ...localization import get_locale
+from matplotlib import colors as mcolors
 
+LINESTYLES = ['-', '--', '-.', ':']
+VALID_COLORS = list(mcolors.BASE_COLORS.keys())+list(mcolors.CSS4_COLORS.keys())
+
+def validate_color(col:str):
+    col = col.strip()
+    if col.startswith("#"):
+        if len(col) != 7:
+            return False
+        for c in col[1:]:
+            if c not in "0123456789ABCDEF":
+                return False
+        return True
+    else:
+        return col in VALID_COLORS
 
 def fine_line(axes, label):
     for line in axes.get_lines():
@@ -17,20 +33,26 @@ class StyleDialog(Dialog):
         self._stylevar = None
         super().__init__(master)
 
-    def add_entry(self, parent, key, initial_var):
+    def add_entry(self, parent, key, initial_var, values=None):
         tk.Label(parent,text=get_locale(key)).grid(row=self._grid_index,column=0, sticky="nsew")
         var = tk.StringVar(parent)
         var.set(initial_var)
-        tk.Entry(parent,textvariable=var).grid(row=self._grid_index,column=1, sticky="nsew")
+        if values is None:
+            tk.Entry(parent,textvariable=var).grid(row=self._grid_index,column=1, sticky="nsew")
+        else:
+            ttk.Combobox(parent, textvariable=var, values=values,
+                         state="readonly").grid(row=self._grid_index, column=1, sticky="nsew")
         self._grid_index += 1
         return var
 
     def body(self, master: tk.Frame) -> tk.Misc | None:
         self._colorvar = self.add_entry(master, "app.popup_plot.dialog.color", self.line_artist.get_color())
-        self._stylevar = self.add_entry(master, "app.popup_plot.dialog.color", self.line_artist.get_linestyle())
+        self._stylevar = self.add_entry(master, "app.popup_plot.dialog.color", self.line_artist.get_linestyle(),
+                                        LINESTYLES)
 
     def apply(self) -> None:
         line = fine_line(self.axes, self.line_artist.get_label())
         if line:
-            line.set_color(self._colorvar.get())
+            if validate_color(self._colorvar.get()):
+                line.set_color(self._colorvar.get())
             line.set_linestyle(self._stylevar.get())
